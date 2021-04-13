ada960080a8e9121504bbd60089993fa8eaf9fc3
Phong Luu <huynhpluu@gmail.com>
2021-04-13 16:14:40 +0700

    added: render Keyboard component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import "./App.css";
 import { Footer } from "./components/Footer/Footer";
 import { Logo } from "./components/Logo/Logo";
 import styles from "./App.module.css";
+import { Keyboard } from "./components/Keyboard/Keyboard";
 
 function App() {
   return (
     <div className={styles.app}>
       <Logo />
-      <main className={styles.content}></main>
+      <main className={styles.content}>
+        <Keyboard />
+      </main>
       <Footer />
     </div>
   );
